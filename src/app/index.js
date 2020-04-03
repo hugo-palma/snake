dae3770906d6543a72a6ad9ebb8323891e42f234
Hugo Palma <hugodobe_@hotmail.com>
2020-04-02 23:27:11 -0600

    adding replay of high scores

--- src/app/index.js
+++ src/app/index.js
@@ -2,15 +2,18 @@
 import Board from './components/board.js';
 import Score from './components/score.js'
 import Controls from './components/Controls'
+import HighScoresTable from "./components/highScoresTable";
 import ReactDOM from 'react-dom'
 import ScoreModel from './mobx-model/scoreModel'
 import ControlsModel from './mobx-model/controlsModel'
+import HighScoresTableModel from "./mobx-model/highScoresTableModel";
 import {Provider} from 'mobx-react'
 import highScoreProcess from './process/proc.highScore'
 
 let scoreModel = ScoreModel.create({score: 2});
-let controlsModel = ControlsModel.create({paused: false, backwards: false});
-let highScoresTable = highScoreProcess.getHighScores();
+let controlsModel = ControlsModel.create({paused: true, backwards: false});
+let highScoresTableModel = HighScoresTableModel.create({replaying: false});
 
-ReactDOM.render(<Provider scoreModel={scoreModel} controlsModel={controlsModel}>
-    <Board/><Score/><Controls/></Provider>, document.getElementById('snakeBoardContainer'));
+
+ReactDOM.render(<Provider scoreModel={scoreModel} controlsModel={controlsModel} highScoresTableModel={highScoresTableModel}>
+    <Board/><Controls/><Score/><HighScoresTable/></Provider>, document.getElementById('snakeBoardContainer'));
--- src/app/process/proc.highScore.js
+++ src/app/process/proc.highScore.js
@@ -1,15 +1,15 @@
 //import request from 'request-promise'
 import axios from 'axios';
 import regeneratorRuntime from 'regenerator-runtime'
+import HighScoresTableModel from "../mobx-model/highScoresTableModel";
 
 const API_URL = 'http://localhost:3000/api';
 const HIGH_SCORE_URL = '/highScore';
-async function getHighScores() {
-    console.log('Obtaining high scores')
+async function getHighScores(highScoresTableModel) {
     const response = await axios.get(API_URL + HIGH_SCORE_URL);
     const highScores = response.data;
-    console.log('scores');
-    console.log(highScores)
+    highScoresTableModel.setHighScoresArrayFroDbmObject(highScores);
+    return highScoresTableModel
 }
 async function postHighScore(email, scoreValue, states){
     const highScoreRequest = {
